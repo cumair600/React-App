3494733689c20609739befabeaf89166d59a59ed
Umair Afzal <cumair600@gmail.com>
2020-07-20 01:05:36 +0500

    Updates

---
+++ precache-manifest.47dbfba183e2928b00ca4d84db3391e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d51fbde925dfd4604dcd114fffb2d4",
+    "url": "/React-App/index.html"
+  },
+  {
+    "revision": "8c30710dad2dce4438d2",
+    "url": "/React-App/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1d411ee13cec0824c2a7",
+    "url": "/React-App/static/js/2.8626492a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-App/static/js/2.8626492a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c30710dad2dce4438d2",
+    "url": "/React-App/static/js/main.b0e3efcb.chunk.js"
+  },
+  {
+    "revision": "2570c3c322102d5dbc98",
+    "url": "/React-App/static/js/runtime-main.b8e43cf6.js"
+  }
+]);
